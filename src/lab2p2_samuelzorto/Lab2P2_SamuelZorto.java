d2a2cca2ef341480c6bfafb122ca0793bc73faf9
Samuel <samuelzorto@gmail.com>
2023-01-27 17:36:32 -0600

    Se puede modificar casas/edificios/solares

--- src/lab2p2_samuelzorto/Lab2P2_SamuelZorto.java
+++ src/lab2p2_samuelzorto/Lab2P2_SamuelZorto.java
@@ -19,9 +19,9 @@ public class Lab2P2_SamuelZorto {
         boolean rep = true;
         boolean user = true; //admin
         int noUser = 0;
-        
-        do{
-            
+
+        do {
+
             System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             System.out.println(usu.get(noUser).toString());
             System.out.println("Elija una opcion de lo que desea hacer:");
@@ -32,38 +32,37 @@ public class Lab2P2_SamuelZorto {
             System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             int menuu = entrada.nextInt();
             System.out.println("");
-            
-            switch(menuu){
+
+            switch (menuu) {
                 case 1:
-                    if(user == true){
+                    if (user == true) {
                         regis(noUser, usu, Inmuebles);
-                    }else{
-                        System.out.println("Solo un administrador puede acceder esta opcion"); 
+                    } else {
+                        System.out.println("Solo un administrador puede acceder esta opcion");
                     }
-                    
+
                     break;
                 case 2:
                     break;
                 case 3:
                     System.out.println("Ya tiene cuenta? Y/N");
                     char tiene = entrada.next().charAt(0);
-                    switch(tiene){
+                    switch (tiene) {
                         case 'y', 'Y':
                             System.out.print("Igrese su Usuario");
                             String usuario = entrada.next();
                             System.out.print("Igrese su Contraseña: ");
                             String pass = entrada.next();
-                            
+
                             for (Usuario t : usu) {
-                                if(t.getUser().equals(usuario) && t.getPass().equals(pass)){
+                                if (t.getUser().equals(usuario) && t.getPass().equals(pass)) {
                                     noUser = usu.indexOf(t);
                                     user = t.isAdmin();
-                                }else{
+                                } else {
                                     System.out.println("El usuario/contraseña que ingreso esta incorrecto");
                                 }
                             }
-                            
-                            
+
                             break;
                         case 'n', 'N':
                             System.out.print("Igrese su Nombre: ");
@@ -74,7 +73,7 @@ public class Lab2P2_SamuelZorto {
                             usuario = entrada.next();
                             System.out.print("Igrese su Contraseña: ");
                             pass = entrada.next();
-                            
+
                             usu.add(new Usuario(nomb, edad, usuario, pass, false));
                             System.out.println("Su cuenta fue creada existosamente. Por favor vaya a log in con su nueva cuenta.");
                             break;
@@ -84,11 +83,11 @@ public class Lab2P2_SamuelZorto {
                     rep = false;
                     break;
             }
-        }while(rep);
-        
+        } while (rep);
+
     }
-    
-    public static void regis(int noUser, ArrayList usu, ArrayList Inmuebles){
+
+    public static void regis(int noUser, ArrayList usu, ArrayList Inmuebles) {
         Scanner entrada = new Scanner(System.in);
         Scanner entrad = new Scanner(System.in);
         System.out.println("[1]Crear Casas/Edificios/Solares\n"
@@ -98,14 +97,14 @@ public class Lab2P2_SamuelZorto {
                 + "[5]Vender Casas/Edificios/Solares\n");
         int eli = entrada.nextInt();
         int inm;
-        switch(eli){
+        switch (eli) {
             case 1:
                 System.out.println("");
                 System.out.println("[1] Casa");
                 System.out.println("[2] Edificio");
                 System.out.println("[3] Solar");
                 inm = entrada.nextInt();
-                switch(inm){
+                switch (inm) {
                     case 1:
                         System.out.print("Ingrese el numero de casa: ");
                         int noC = entrada.nextInt();
@@ -148,27 +147,27 @@ public class Lab2P2_SamuelZorto {
                 System.out.println("[3] Solar");
                 System.out.println("[4] Todos");
                 inm = entrada.nextInt();
-                switch(inm){
+                switch (inm) {
                     case 1:
                         for (Object t : Inmuebles) {
-                            if(t instanceof Casa){
-                                System.out.println(t.toString());
+                            if (t instanceof Casa) {
+                                System.out.println(Inmuebles.indexOf(t) + " " + t.toString());
                             }
                         }
                         System.out.println("");
                         break;
                     case 2:
                         for (Object t : Inmuebles) {
-                            if(t instanceof Edificios){
-                                System.out.println(t.toString());
+                            if (t instanceof Edificios) {
+                                System.out.println(Inmuebles.indexOf(t) + " " + t.toString());
                             }
                         }
                         System.out.println("");
                         break;
                     case 3:
                         for (Object t : Inmuebles) {
-                            if(t instanceof Casa){
-                                System.out.println(t.toString());
+                            if (t instanceof Casa) {
+                                System.out.println(Inmuebles.indexOf(t) + " " + t.toString());
                             }
                         }
                         System.out.println("");
@@ -184,23 +183,96 @@ public class Lab2P2_SamuelZorto {
                 System.out.println("[3] Solar");
                 inm = entrada.nextInt();
                 int elij;
-                switch(inm){
+                switch (inm) {
                     case 1:
-                        System.out.println("Elija lo que desee modificar:\n"
-                                + "[1] No. de casa\n"
-                                + "[2] No. de bloque\n"
-                                + "[3] Color\n"
-                                + "[4] Ancho y Largo\n"
-                                + "[5] No de Baños\n"
-                                + "[6] No. de Cuartos");
-                        elij = entrada.nextInt();
-                        switch(elij){
-                            
+                        System.out.print("ELija posicion de la casa que desea modificar: ");
+                        int pos = entrada.nextInt();
+                        if (Inmuebles.get(pos) instanceof Casa) {
+
+                            System.out.println("Elija lo que desee modificar:\n"
+                                    + "[1] No. de casa\n"
+                                    + "[2] No. de bloque\n"
+                                    + "[3] Color\n"
+                                    + "[4] Ancho y Largo\n"
+                                    + "[5] No de Baños\n"
+                                    + "[6] No. de Cuartos");
+                            elij = entrada.nextInt();
+                            switch (elij) {
+                                case 1:
+                                    System.out.print("Ingrese el numero de casa: ");
+                                    int noC = entrada.nextInt();
+                                    ((Casa) Inmuebles.get(pos)).setNoC(noC);
+                                    break;
+                                case 2:
+                                    System.out.print("Ingrese el numero de bloque: ");
+                                    int noB = entrada.nextInt();
+                                    ((Casa) Inmuebles.get(pos)).setNoB(noB);
+                                    break;
+                                case 3:
+                                    System.out.println("Ingrese el color: ");
+                                    Color color = JColorChooser.showDialog(null, "Seleccione color", Color.black);
+                                    ((Casa) Inmuebles.get(pos)).setColor(color);
+                                    break;
+                                case 4:
+                                    System.out.print("Ingrese el ancho: ");
+                                    int ancho = entrada.nextInt();
+                                    ((Casa) Inmuebles.get(pos)).setAncho(ancho);
+                                    System.out.print("Ingrese el largo: ");
+                                    int largo = entrada.nextInt();
+                                    ((Casa) Inmuebles.get(pos)).setLargo(largo);
+                                    break;
+                                case 5:
+                                    System.out.print("Ingrese el numero de baños: ");
+                                    int noDB = entrada.nextInt();
+                                    ((Casa) Inmuebles.get(pos)).setNoDB(noDB);
+                                    break;
+                                case 6:
+                                    System.out.println("Ingrese el numero de cuartos: ");
+                                    int noDC = entrada.nextInt();
+                                    ((Casa) Inmuebles.get(pos)).setNoDC(noDC);
+                                    break;
+                            }
                         }
                         break;
                     case 2:
+                        System.out.print("ELija posicion de la casa que desea modificar: ");
+                        pos = entrada.nextInt();
+                        if (Inmuebles.get(pos) instanceof Casa) {
+
+                            System.out.println("Elija lo que desee modificar:\n"
+                                    + "[1] No. de pisos\n"
+                                    + "[2] Cantidad de locales\n"
+                                    + "[3] Direccion por referencia");
+                            elij = entrada.nextInt();
+                            switch (elij) {
+                                case 1:
+                                    System.out.println("Ingrese el numero de pisos: ");
+                                    int noP = entrada.nextInt();
+                                    ((Edificios) Inmuebles.get(pos)).setPisos(noP);
+                                    break;
+                                case 2:
+                                    System.out.println("Ingrese la cantidad de locales: ");
+                                    int locales = entrada.nextInt();
+                                    ((Edificios) Inmuebles.get(pos)).setLocales(locales);
+                                    break;
+                                case 3:
+                                    System.out.println("Infrese la direccion por referecia: ");
+                                    String dirRef = entrad.nextLine();
+                                    ((Edificios) Inmuebles.get(pos)).setDirRef(dirRef);
+                                    break;
+                            }
+                        }
                         break;
                     case 3:
+                        System.out.print("ELija posicion de la casa que desea modificar: ");
+                        pos = entrada.nextInt();
+                        if (Inmuebles.get(pos) instanceof Casa) {
+                            System.out.println("Igrese el Ancho y Largo: ");
+                            int ancho = entrada.nextInt();
+                            ((Solares) Inmuebles.get(pos)).setAncho(ancho);
+                            int largo = entrada.nextInt();
+                            ((Solares) Inmuebles.get(pos)).setLargo(largo);
+                        }
                         break;
                 }
                 break;
@@ -210,7 +282,7 @@ public class Lab2P2_SamuelZorto {
                 System.out.println("[2] Edificio");
                 System.out.println("[3] Solar");
                 inm = entrada.nextInt();
-                switch(inm){
+                switch (inm) {
                     case 1:
                         break;
                     case 2:
@@ -225,7 +297,7 @@ public class Lab2P2_SamuelZorto {
                 System.out.println("[2] Edificio");
                 System.out.println("[3] Solar");
                 inm = entrada.nextInt();
-                switch(inm){
+                switch (inm) {
                     case 1:
                         break;
                     case 2:
@@ -236,5 +308,5 @@ public class Lab2P2_SamuelZorto {
                 break;
         }
     }
-    
+
 }
