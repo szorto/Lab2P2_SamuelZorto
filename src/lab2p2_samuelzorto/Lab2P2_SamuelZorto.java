20eaeba64d765805956eb5ba3e1d5d6485918dc8
Samuel <samuelzorto@gmail.com>
2023-01-27 13:40:43 -0600

    Subiendo lso primeros archivos a la reposicion

---
+++ src/lab2p2_samuelzorto/Lab2P2_SamuelZorto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab2p2_samuelzorto;
+
+/**
+ *
+ * @author szorto
+ */
+public class Lab2P2_SamuelZorto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
